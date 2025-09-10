ec409f9322f4632be5d88afa3e0dc24c1761efc1
Jaypee-Mangilit <jaypeemangilit001@gmail.com>
2025-09-10 10:01:52 +0800

    Add some cool styling with styled-components

---
+++ src/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MainApp = styled.div`
+  display: flex;
+  flex-direction: column; /* stack items vertically */
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #f5f5f5;
+  font-size: 20px;
+`;
