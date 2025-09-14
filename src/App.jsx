480dc76eec58620a7293404c1f7406c57f1f74da
Jaypee-Mangilit <jaypeemangilit001@gmail.com>
2025-09-14 11:24:13 +0800

    Update styles for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { MainApp } from "./styled";
+import {Heading, Text, Button} from '@chakra-ui/react';
 
 export const App = () => {
   const storageKeyName = "count";
@@ -16,8 +17,12 @@ export const App = () => {
 
   return (
     <MainApp>
-      Count Me
-      <button onClick={() => addNumber(count)}>count is {count}</button>
+      <Heading>ang baho mo</Heading>
+      <Text>talaga ba?</Text>
+      <Text color={"tomato"}>Count Me</Text>
+      <Button colorScheme="teal" variant="solid" onClick={() => addNumber(count)}>count is{count}
+
+      </Button>
     </MainApp>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { App } from "./App"; // if you saved that code in App.jsx
+
+const theme = extendTheme({
+  color: {
+    brand: {
+      100: "#f7fafc",
+        // ...
+      900: "#1a202c",
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
